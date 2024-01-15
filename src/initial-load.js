f0faec7cc737504875499baba55a3c0296789222
tostimontes <147332922+tostimontes@users.noreply.github.com>
2024-01-15 10:32:11 +0100

    initial file and directory setup

---
+++ src/initial-load.js
@@ -0,0 +1,5 @@
+import createDOMElement from "C:/Users/Aitor/Google Drive/Kode/projects/reusables/JavaScript/DOMElementCreator.js";
+
+const restaurantTitle = createDOMElement("h1", {
+    class: "restaurant-title", id: "restaurant-title"
+});
