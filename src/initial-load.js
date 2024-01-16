8f4d588a30e21142ec338568f9f3a1dd76ed8a64
tostimontes <147332922+tostimontes@users.noreply.github.com>
2024-01-16 12:11:28 +0100

    feat: home_tab added and styled
    - Fixed main layout problems

--- src/initial-load.js
+++ src/initial-load.js
@@ -11,11 +11,10 @@ function createInitialUI() {
       href: "https://use.fontawesome.com/releases/v5.15.4/css/all.css",
     }
   );
-  const restaurantTitle = createDOMElement(
+  const heroTitle = createDOMElement(
     "h1",
     {
-      class: "restaurant_title",
-      id: "restaurant_title",
+      id: "hero_title",
     },
     "Bar Armengol"
   );
@@ -51,7 +50,8 @@ function createInitialUI() {
     { class: "", id: "test_p" },
     "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco labori\ns nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est loluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est loluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est loluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit animoluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est loluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est loluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est l id est loluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.  Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperia\n m, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.\n  Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
   );
-  const footer = createDOMElement("footer", { id: "footer"}, "by tostimontes");
+  const footer = createDOMElement("footer", { id: "footer"}, "by ");
+  const nameSpan = createDOMElement("span", {class: "footer", id: "name_span"}, "tostimontes");
   const githubLink = createDOMElement(
     "a",
     { href: "https://github.com/tostimontes/restaurant-page" }
@@ -64,15 +64,16 @@ function createInitialUI() {
   // gironaInterior.innerHTML = `<a href="https://www.freepik.com/free-photo/old-catalan-village-castellar-de-la-ribera_1469621.htm#query=castell&position=45&from_view=search&track=sph&uuid=2252c821-7baa-4e57-9bef-da7cf873a1eb">Image by bearfotos</a> on Freepik`;
 
   navbar.append(homeTab, menuTab, contactTab);
-  header.append(restaurantTitle, navbar);
+  header.append(heroTitle, navbar);
   // mainContainer.append(testP);
+  mainDiv.append(mainContainer, footer)
   githubLink.append(githubIcon);
-  footer.append(githubLink)
+  footer.append(nameSpan, githubLink)
 
   const body = document.querySelector("body");
   const head = document.querySelector("head");
   head.append(falink);
-  body.append(header, mainDiv, mainContainer, footer);
+  body.append(header, mainDiv);
 }
 
 export { createInitialUI };
